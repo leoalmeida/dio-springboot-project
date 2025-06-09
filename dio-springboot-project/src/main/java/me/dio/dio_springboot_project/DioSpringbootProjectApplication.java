1746d1ce6c8a4a1c12fa6d0e4d2f1aa4cc6216fe
Leonardo Almeida <leoalmeida.rj@gmail.com>
2025-06-09 18:07:37 -0300

    Initial commit

---
+++ dio-springboot-project/src/main/java/me/dio/dio_springboot_project/DioSpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package me.dio.dio_springboot_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioSpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioSpringbootProjectApplication.class, args);
+	}
+
+}
---
+++ dio-springboot-project/src/test/java/me/dio/dio_springboot_project/DioSpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.dio_springboot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSpringbootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
